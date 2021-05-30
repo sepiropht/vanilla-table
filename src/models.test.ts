bc1adcd3ad3749c9f056f2755ca1ea93f997c4d0
William Mbotta <sergembotta@mailoo.org>
2021-05-30 15:23:41 +0200

    add test

---
+++ src/models.test.ts
@@ -0,0 +1,65 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { Data, filterByAge, getRange } from './models'
+
+const data: Data = [
+  {
+    firstName: 'first',
+    lastName: 'last',
+    eyeColor: 'brown',
+    age: 20,
+  },
+  {
+    firstName: 'first',
+    lastName: 'last',
+    eyeColor: 'brown',
+    age: 26,
+  },
+  {
+    firstName: 'first',
+    lastName: 'last',
+    eyeColor: 'brown',
+    age: 32,
+  },
+]
+
+test('should get range according to age', () => {
+  expect(getRange('[20-25]')).toStrictEqual([20, 25])
+  expect(getRange('[26-30]')).toStrictEqual([26, 30])
+  expect(getRange('[31-35]')).toStrictEqual([31, 35])
+  expect(getRange('[36-41]')).toStrictEqual([36, 41])
+})
+
+test('should throw error', () => {
+  expect(() => getRange('[20, 26]')).toThrowError('Wrong range')
+})
+
+test('should filter collection by age', () => {
+  expect(filterByAge(data, [20, 25])).toStrictEqual([
+    {
+      firstName: 'first',
+      lastName: 'last',
+      eyeColor: 'brown',
+      age: 20,
+    },
+  ])
+  expect(filterByAge(data, [26, 30])).toStrictEqual([
+    {
+      firstName: 'first',
+      lastName: 'last',
+      eyeColor: 'brown',
+      age: 26,
+    },
+  ])
+
+  expect(filterByAge(data, [31, 35])).toStrictEqual([
+    {
+      firstName: 'first',
+      lastName: 'last',
+      eyeColor: 'brown',
+      age: 32,
+    },
+  ])
+})
