6697600457b21ece0a9c5763a11723bcaffb04c8
William Mbotta <sergembotta@mailoo.org>
2021-05-30 21:21:00 +0200

    move pagination

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import json from './datas.json'
 import { render } from './ui/render'
 import parseUrl from './utils/parseUrl'
 import { Data, isEyeColor, filterByAge, getRange } from './models'
+import { pagination } from './ui/pagination'
 
 let data: Data = json.map(({ name, eyeColor, age, email, company, phone }) => ({
   firstName: name.first.toLowerCase(),
@@ -36,43 +37,7 @@ for (let [query, value] of entries) {
   }
   data = data.filter((elem) => elem[query] === value)
 }
-const prev: HTMLButtonElement = document.querySelector('.prev')
-const next: HTMLButtonElement = document.querySelector('.next')
-if (data.length < 11) {
-  next.disabled = true
-  prev.disabled = true
-}
-let stateSlider = 0
 
-next.addEventListener('click', () => {
-  if (10 * stateSlider > data.length) {
-    return
-  }
-  stateSlider++
-  const start = stateSlider === 0 ? 0 : 10 * stateSlider
-  const end = start + 11
-  render(data.slice(start, end), start)
-  if (end > data.length) {
-    next.disabled = true
-  } else if (stateSlider > 0) {
-    prev.disabled = false
-  }
-})
-
-prev.addEventListener('click', () => {
-  if (stateSlider === 0) {
-    return
-  }
-  stateSlider--
-  const start = stateSlider === 0 ? 0 : 10 * stateSlider
-  const end = start + 11
-  render(data.slice(start, end), start)
-
-  if (stateSlider === 0) {
-    prev.disabled = true
-  } else if (10 * stateSlider < data.length) {
-    next.disabled = false
-  }
-})
+pagination(data)
 
 render(data.slice(0, 10))
